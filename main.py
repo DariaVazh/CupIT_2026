0aee1efd74f945d2698715c5d1137451954a8918
btwnastya2 <nastya.btw@yahoo.com>
2026-03-15 00:44:46 +0300

    название поменяла

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 main_page = st.Page("app.py", title="Главная")
-an_page = st.Page("top.py", title="Топ доменов")
+an_page = st.Page("top.py", title="Обычный vs Генеративный")
 
 pg = st.navigation([main_page, an_page])
 pg.run()

a755ba3fa815eac61e13d293aa8f22c3add77c7b
btwnastya2 <nastya.btw@yahoo.com>
2026-03-15 18:47:52 +0300

    апдейт какой-то

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from streamlit import title
 
 main_page = st.Page("app.py", title="Главная")
 an_page = st.Page("top.py", title="Обычный vs Генеративный")
+t_page = st.Page('model.py', title='Экономическая модель')
 
-pg = st.navigation([main_page, an_page])
+
+pg = st.navigation([main_page, an_page, t_page])
 pg.run()
